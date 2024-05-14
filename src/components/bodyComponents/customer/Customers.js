f7d5dbea820177835042a1a074701f042ba6f2c5
DevangGarg <garg.devangrocks@gmail.com>
2024-05-14 13:56:44 +0530

    Created git repository front end for inventory project

---
+++ src/components/bodyComponents/customer/Customers.js
@@ -0,0 +1,190 @@
+import productList from "../inventory/productList";
+
+const customers = [{
+    id:1,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:2,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:3,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:4,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:5,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:6,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:7,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:8,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:9,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:10,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:11,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:12,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:13,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:14,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:15,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:16,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:17,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:18,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:19,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:20,firstName: "INVENT",
+    lastName: "CUSTOMER",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}];
+export default customers;
